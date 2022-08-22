1b8d8242d89fb79f031cb8c1655f0ba5baa1c174
Usama Mehmood <usama.mehmood@qavitechnologies.com>
2022-08-22 13:19:08 -0700

    first commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  newPost = 'No Content';
+  enteredValue = 'Nothing';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddPost() {
+    this.newPost = this.enteredValue;;
+  }
+
+}
